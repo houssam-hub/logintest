410c37e6c6e140b45d536fa7ef818877c0175573
houssam-hub <h.abbaseddine@gmail.com>
2025-07-06 20:04:08 +0100

    Add files via upload

---
+++ LOGIN/validation.js
@@ -0,0 +1,5 @@
+const form =  document.getElementById('form');
+const firstname_input = document.getElementById('firstname-input');
+const email_input = document.getElementById('email-input');
+const password_input =  document.getElementById('password-input');
+const repeat_password_input =  document.getElementById('repeat-password-input');
